9e378956557cddd3c2566675ff8165efd1443d69
abdelkouddous <Abdelkouddous.ElAlami@progressoft.com>
2024-12-29 13:20:00 +0100

    Create DTOs for request/response handling

---
+++ gestion-stock/src/main/java/com/tricol/gestionstock/dto/auth/PermissionResponse.java
@@ -0,0 +1,18 @@
+package com.tricol.gestionstock.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PermissionResponse {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String category;
+}
